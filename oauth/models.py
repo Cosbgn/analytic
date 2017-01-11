5b5ccc7331b58e88d01e721a5594a34f29646519
Costantin <Cosbgn@gmail.com>
2017-01-11 21:16:00 +0100

    update oauth app

--- oauth/models.py
+++ oauth/models.py
@@ -1,11 +1,22 @@
+
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
-
-###### Google oAuth ##########
 from django.contrib.auth.models import User
 from oauth2client.contrib.django_util.models import CredentialsField
 
 class CredentialsModel(models.Model):
     user_id = models.OneToOneField(User)
     credential = CredentialsField()
+
+    class Meta:
+        db_table = 'credentials'
+
+    def __str__(self):
+    	return 'creds'
+
+# to be able to see credentials in admin
+class CredentialsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CredentialsModel, CredentialsAdmin)
--- oauth/views.py
+++ oauth/views.py
@@ -1,43 +1,97 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.db import models
-from django.views.generic import TemplateView
-from oauth2client.contrib.django_util import decorators # Google oAuth
-# oAuth
-from .models import CredentialsModel # To get credentials
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse_lazy
+from django.template.context import RequestContext
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User, UserManager
+from django.shortcuts import render_to_response, redirect, render
+from django.http import (HttpResponse, HttpResponseBadRequest, HttpResponseRedirect)
+
+# to get credentials
+from .models import CredentialsModel
+
+import pickle
+from YamJam import yamjam
+from apiclient import discovery
+from django.conf import settings
+from oauth2client.contrib import xsrfutil
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.contrib.django_util.storage import DjangoORMStorage
-from django.conf import settings
 
-# Flow to authenticate the user
+
 def get_flow(request):
     flow = OAuth2WebServerFlow(
-        client_id = settings.GOOGLE_OAUTH2_CLIENT_ID,
-        client_secret = settings.GOOGLE_OAUTH2_CLIENT_SECRET,
-        scope = settings.GOOGLE_OAUTH2_SCOPES,
-        redirect_uri = settings.GOOGLE_REDIRECT,
-        access_type='offline',
-        prompt="consent",
+        client_id=settings.GOOGLE_OAUTH2_CLIENT_ID,
+        client_secret=settings.GOOGLE_OAUTH2_CLIENT_SECRET,
+        scope= settings.GOOGLE_OAUTH2_SCOPES
+        redirect_uri=settings.GOOGLE_REDIRECT,
+        access_type='offline', # ??? What's this?
+        prompt="consent", # ??? What's this?
     )
 
     return flow
 
-##### Google OAuth #######
+def home(request):
+   context = RequestContext(request, {'request':request, 'user':request.user })
+   return render_to_response('ga/index.html', context=context)
+S
 
-@decorators.oauth_required
-def get_profile_required(request):
-    resp, content = request.oauth.http.request(
-        'https://www.googleapis.com/plus/v1/people/me')
-    return HttpResponse(content)
+def get_creds(request):
+    """
+    builds flow
+    after successful authorizing of app redirects to user auth pages
+    """
+    flow = get_flow(request)
+    flow.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
+                                                       request.user.id)
+    request.session['flow'] = pickle.dumps(flow).decode('iso-8859-1')
+    redirect_uri = flow.step1_get_authorize_url()
 
-@decorators.oauth_enabled
-def get_profile_optional(request):
-    if request.oauth.has_credentials():
-        # this could be passed into a view
-        # request.oauth.http is also initialized
-        return HttpResponse('User email: {}'.format(
-            request.oauth.credentials.id_token['email']))
+    return redirect(redirect_uri)
+
+
+def oauth2redirect(request):
+    """
+    validates user token,
+    saves user credentials into db
+    redirects you to 'viz/' (viz/views.viz)
+    """
+    # Make sure that the request is from who we think it is
+    if not xsrfutil.validate_token(settings.SECRET_KEY,
+                                   request.GET.get('state').encode('utf8'),
+                                   request.user.id):
+        return HttpResponseBadRequest()
+
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    if code:
+        flow = get_flow(request)
+        credentials = flow.step2_exchange(code)
+
+        request.session['creds'] = credentials.to_json()
+        email = credentials.id_token.get("email")
+        user = User.objects.get(email=email)
+
+        # Since we've oauth2'd the user, we should set the backend appropriately
+        # This is usually done by the authenticate() method.
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+
+        # Refresh token is needed for renewing google api access token
+        if credentials.refresh_token:
+            user.refresh_token = credentials.refresh_token
+        user.save()
+
+        storage = DjangoORMStorage(CredentialsModel, 'id', user, 'credentials')
+        storage.put(credentials)
+
+        # Register that the user has successfully logged in
+        auth_login(request, user)
+
+        return redirect('viz')
+
+    elif code is None and error:
+        return HttpResponse(str(error))
     else:
-        return HttpResponse(
-            'Here is an OAuth Authorize link:<a href="{}">Authorize</a>'
-            .format(request.oauth.get_authorize_redirect()))
+        return HttpResponseBadRequest()
